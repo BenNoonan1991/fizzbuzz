e96df2790b8d0ba860f647595b9889b3888e5620
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-03 17:41:45 +0100

    Add tests for buzz, fizzbuzz and other numbers

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,25 @@
+require '../lib/fizzbuzz'
+
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "Fizzbuzz" when passed 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'Fizzbuzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns number when passed 11' do
+    expect(fizzbuzz(11)).to eq 11
   end
 end
